c3dbf35e59b26c5c414f6e0e7467cd092e142a8f
Das-Swarup <swarupd049@gmail.com>
2023-01-11 13:31:31 +0530

    finalizing

--- App.js
+++ App.js
@@ -1,19 +1,262 @@
 import React from "react";
-import {Flex,Spacer,Text, NativeBaseProvider, Box, Heading, Input } from "native-base";
+import { HStack, Center, Flex, Spacer, Text, NativeBaseProvider, Box, Heading, Input, Icon, VStack, Pressable, ScrollView, Avatar, Button } from "native-base";
+import { MaterialIcons, Ionicons, Feather, MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons';
 import styles from './styles';
+
+import { useFonts, Parisienne_400Regular } from '@expo-google-fonts/parisienne';
 
 
 export default function App() {
+
+  const [selected, setSelected] = React.useState(1);
+
+  let [fontsLoaded] = useFonts({
+    Parisienne_400Regular,
+  });
+
   return (
     <NativeBaseProvider>
-      <Box style={styles.header}>
-        <Input style={styles.search} placeholder="Search" placeholderTextColor="black"/>
-        <Flex direction="row" width={300} marginTop={5}>
-          <Text fontSize={25}>Announcements</Text>
-          <Spacer st/>
-          <Text fontSize={25} color="success.500" underline>View All</Text>
+      <ScrollView>
+        <Box style={styles.header}>
+          <VStack alignSelf="center">
+            <Input placeholder="Search" variant="filled" width="250" borderRadius="5" py="1" px="2" fontSize={18} placeholderTextColor="black" color="black" InputLeftElement={<Icon ml="2" size="4" color="black" as={<Ionicons name="ios-search" />} />} />
+          </VStack>
+          <Flex direction="row" width={350} marginTop={8}>
+            <Text fontSize={20}>Announcements</Text>
+            <Spacer />
+            <Text fontSize={20} color="success.500" underline>View All</Text>
+          </Flex>
+
+          <Flex marginTop={8} direction="row" align="center" width={280} backgroundColor="orange.200" padding={3} borderRadius={10}>
+            <Icon backgroundColor="yellow.300" borderRadius={5} as={<Feather name="clock" size={40} color="black" />} />
+            <Spacer />
+            <Flex>
+              <Text>Employeers Are Expected To Clock...</Text>
+              <Text>1 hour ago</Text>
+            </Flex>
+          </Flex>
+
+        </Box>
+        <Box style={styles.headerTwo} marginTop={2}>
+          <Text color="orange.500" style={{ fontFamily: "Parisienne_400Regular", fontSize: 40, }}>Welcome</Text>
+          <HStack space={5} flexWrap={'nowrap'} marginLeft={5} marginRight={5}>
+          <Center>
+              <Flex style={styles.newCard} alignItems="center">
+                <Avatar bg="green.500" source={{
+                  uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                }}>
+                  
+                </Avatar>
+                <Text style={styles.headerText}>Megha</Text>
+                <Text>UX Designer</Text>
+                <Text>Joined Today</Text>
+              </Flex>
+            </Center>
+            <Center>
+              <Flex style={styles.newCard} alignItems="center">
+                <Avatar bg="green.500" source={{
+                  uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                }}>
+                  
+                </Avatar>
+                <Text style={styles.headerText}>Sahil</Text>
+                <Text>UX Designer</Text>
+                <Text>Joined Today</Text>
+              </Flex>
+            </Center>
+            <Center>
+              <Flex style={styles.newCard} alignItems="center">
+                <Avatar bg="green.500" source={{
+                  uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                }}>
+                  
+                </Avatar>
+                <Text style={styles.headerText}>Ravi</Text>
+                <Text>UX Designer</Text>
+                <Text>Joined Today</Text>
+              </Flex>
+            </Center>
+          </HStack>
+        </Box>
+
+        <Box alignItems="center" marginTop={5} marginBottom={5}>
+          <Flex direction="row" alignItems="center" width={320}>
+        <Icon as={MaterialCommunityIcons} name="clock" size="2xl" color="blue.600" />
+        <Spacer/>
+        <Flex>
+          <Text>03/01/23</Text>
+          <Text>Clock In - hh:mm AM</Text>
+          <Text>Clock In - hh:mm PM</Text>
         </Flex>
+        <Spacer/>
+        <Button width={100} backgroundColor="blue.600">Clock In</Button>
+        <Spacer/>
+        <Icon as={FontAwesome} name="angle-right" size="xl"/>
+        </Flex>
+        </Box>
+
+        <Box style={styles.teamBox}>
+          <Flex direction="row" width={350} alignItems="center">
+            <Text fontSize={22} fontWeight={700}>My Team</Text>
+            <Spacer />
+            <Text fontSize={22} color="success.500" underline>View All</Text>
+          </Flex>
+          <Flex>
+            <Spacer />
+            <Flex direction="row" width={300}>
+              <Text>Rashmi</Text><Spacer />
+              <Text>Trainee</Text><Spacer />
+              <Text>Present</Text><Spacer />
+            </Flex>
+            <Spacer />
+            <Flex direction="row" width={300}>
+              <Text>Rashmi</Text><Spacer />
+              <Text>Trainee</Text><Spacer />
+              <Text>Present</Text><Spacer />
+            </Flex>
+            <Spacer />
+            <Flex direction="row" width={300}>
+              <Text>Rashmi</Text><Spacer />
+              <Text>Trainee</Text><Spacer />
+              <Text>Present</Text><Spacer />
+            </Flex>
+            <Spacer />
+            <Flex direction="row" width={300}>
+              <Text>Rashmi</Text><Spacer />
+              <Text>Trainee</Text><Spacer />
+              <Text>Present</Text><Spacer />
+            </Flex>
+            <Spacer />
+            <Flex direction="row" width={300}>
+              <Text>Rashmi</Text><Spacer />
+              <Text>Trainee</Text><Spacer />
+              <Text>Present</Text><Spacer />
+            </Flex>
+            <Spacer />
+            <Flex direction="row" width={300}>
+              <Text>Rashmi</Text><Spacer />
+              <Text>Trainee</Text><Spacer />
+              <Text>Present</Text><Spacer />
+            </Flex>
+          </Flex>
+
+        </Box>
+
+        <Box style={styles.teamBox} >
+          <Flex direction="row" width={350} alignItems="center">
+            <Text fontSize={22} fontWeight={700}>Quick Links</Text>
+          </Flex>
+          <Flex>
+            <Spacer />
+            <Flex direction="row" width={350}>
+              <Text>Holiday Calender</Text><Spacer />
+              <Text>Leave Policy</Text><Spacer />
+              <Text>Payroll Manual</Text><Spacer />
+            </Flex>
+          </Flex>
+        </Box>
+
+        <Box style={styles.teamBox} >
+          <Flex direction="row" width={350} alignItems="center">
+            <Text fontSize={22} fontWeight={700}>Celebrations</Text>
+          </Flex>
+          <Flex>
+            <Spacer />
+            <Flex direction="row" width={350}>
+              <Text>Holiday Calender</Text><Spacer />
+              <Text>Leave Policy</Text><Spacer />
+              <Text>Payroll Manual</Text><Spacer />
+            </Flex>
+            <Flex direction="row" width={350}>
+              <Text>Holiday Calender</Text><Spacer />
+              <Text>Leave Policy</Text><Spacer />
+              <Text>Payroll Manual</Text><Spacer />
+            </Flex>
+            <Flex direction="row" width={350}>
+              <Text>Holiday Calender</Text><Spacer />
+              <Text>Leave Policy</Text><Spacer />
+              <Text>Payroll Manual</Text><Spacer />
+            </Flex>
+            <Flex direction="row" width={350}>
+              <Text>Holiday Calender</Text><Spacer />
+              <Text>Leave Policy</Text><Spacer />
+              <Text>Payroll Manual</Text><Spacer />
+            </Flex>
+          </Flex>
+        </Box>
+
+        <Box style={styles.teamBox} marginBottom={20}>
+          <Flex direction="row" width={350} alignItems="center">
+            <Text fontSize={22} fontWeight={700}>My Tasks</Text>
+          </Flex>
+          <Flex>
+            <Spacer />
+            <Flex direction="row" width={350}>
+              <Text>Attendance</Text><Spacer />
+              <Text>17</Text><Spacer />
+              <Text>View</Text><Spacer />
+            </Flex>
+            <Flex direction="row" width={350}>
+              <Text>Attendance</Text><Spacer />
+              <Text>17</Text><Spacer />
+              <Text>View</Text><Spacer />
+            </Flex>
+            <Flex direction="row" width={350}>
+              <Text>Attendance</Text><Spacer />
+              <Text>17</Text><Spacer />
+              <Text>View</Text><Spacer />
+            </Flex>
+            <Flex direction="row" width={350}>
+              <Text>Attendance</Text><Spacer />
+              <Text>17</Text><Spacer />
+              <Text>View</Text><Spacer />
+            </Flex>
+          </Flex>
+        </Box>
+
+
+      </ScrollView>
+
+
+      {/* Bottom buttons */}
+
+      <Box flex={1} bg="white" safeAreaTop width="100%" alignSelf="center" bottom={0} position="absolute">
+
+        <HStack bg="primary.100" alignItems="center" safeAreaBottom shadow={6}>
+          <Pressable cursor="pointer" opacity={selected === 0 ? 1 : 0.5} py="3" flex={1} onPress={() => setSelected(0)}>
+            <Center>
+              <Icon mb="1" as={<MaterialCommunityIcons name={selected === 0 ? 'home' : 'home-outline'} />} color="success.700" size="xl" />
+              <Text bold color="success.700" fontSize="20">
+                Home
+              </Text>
+            </Center>
+          </Pressable>
+          <Pressable cursor="pointer" opacity={selected === 1 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(1)}>
+            <Center>
+            <Avatar bg="green.500" source={{
+                  uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                }}>
+                  AJ
+                </Avatar>
+              <Text bold color="success.700" fontSize="12">
+                My Space
+              </Text>
+            </Center>
+          </Pressable>
+          <Pressable cursor="pointer" opacity={selected === 3 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(3)}>
+            <Center>
+              <Icon mb="1" as={<MaterialIcons name={selected === 3 ? 'notifications' : 'notifications-none'} />}  color="success.700" size="xl" />
+              <Text bold color="success.700" fontSize="12">
+                Notifications
+              </Text>
+            </Center>
+          </Pressable>
+        </HStack>
       </Box>
+
+
+
+      {/* Bottom buttons */}
     </NativeBaseProvider>
   );
 }
--- styles.js
+++ styles.js
@@ -1,11 +1,23 @@
 import { StyleSheet } from 'react-native';
 
+
+
+
 export default StyleSheet.create({
+  
   header: {
     backgroundColor: 'lightgrey',
-    height: 200,
+    height: 250,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  headerTwo: {
+    backgroundColor: 'lightgrey',
+    height: 250,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomLeftRadius:30,
+    borderBottomRightRadius:30,
   },
   headerText: {
     color: '#000',
@@ -20,7 +32,9 @@ export default StyleSheet.create({
     color: '#000',
   },
   search:{
-    maxWidth:200,
+    maxWidth:250,
+    height: 32,
+    padding:0,
     backgroundColor:"white",
     borderRadius: 5,
     fontSize:18,
@@ -29,5 +43,33 @@ export default StyleSheet.create({
   announcementBox:{
 
     flexDirection:"row",
+  },
+  teamBox:{
+    padding:10,
+    alignItems:"center",
+    marginTop:12,
+    marginLeft: 10,
+    marginRight:10,
+    borderWidth: 2,
+    borderRadius: 12,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.4,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  newCard:{
+    backgroundColor:"white",
+    padding:5,
+    alignItems:"center",
+    justifyContent:"center",
+    borderColor:"yellow",
+    borderWidth:1,
+    borderRadius:10,
+    elevation:20,
+    shadowColor: '#000',
   }
+
 });
